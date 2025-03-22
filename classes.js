44a71eeeb33b88654621e57278358fd53ec38067
sanjaydokula <sanjaydokula@gmail.com>
2025-03-23 01:39:12 +0530

    class mappings

---
+++ classes.js
@@ -0,0 +1,20 @@
+// classMappings.js  
+const itoc = {
+    "0": "The Eiffel Tower",
+    "1": "airplane",
+    "2": "ambulance",
+    "3": "anvil",
+    "4": "apple",
+    "5": "axe",
+    "6": "banana",
+    "7": "barn",
+    "8": "baseball bat",
+    "9": "baseball",
+    "10": "basketball",
+    "11": "bench",
+    "12": "bicycle"
+  };
+  
+  // Alternative: Export as a single object
+export default {itoc};
+  
